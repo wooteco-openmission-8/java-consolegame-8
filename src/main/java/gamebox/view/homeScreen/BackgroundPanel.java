679a15e90072232af69c67e85192f820d4e09365
kimsz123456 <kimsz123456@gmail.com>
2025-11-10 19:26:30 +0900

    fix: 패널 구조 변경
    메인 패널 = 백그라운드 패널 + 콘텐트 패널로 변경

--- src/main/java/gamebox/view/homeScreen/BackgroundPanel.java
+++ src/main/java/gamebox/view/homeScreen/BackgroundPanel.java
@@ -1,15 +1,19 @@
 package gamebox.view.homeScreen;
 
+import java.awt.event.ActionListener;
 import javax.swing.*;
 import java.awt.*;
 
 public class BackgroundPanel extends JPanel {
-    private static final JLabel title = new JLabel("GameBox");
-    private static final JLabel selectGame = new JLabel("게임을 선택하세요.");
+    private final JLabel title = new JLabel("GameBox");
+    private final JLabel selectGame = new JLabel("게임을 선택하세요.");
+    private final JButton homeButton = new JButton("홈버튼");
 
     public BackgroundPanel(){
         setBackground(Color.white);
-        setLayout(new GridLayout(2, 1));
+        setLayout(new GridLayout(3, 1));
+        selectGame.setVisible(true);
+        homeButton.setVisible(false);
         alignTexts();
         addComponents();
     }
@@ -17,10 +21,23 @@ public class BackgroundPanel extends JPanel {
     private void alignTexts(){
         title.setHorizontalAlignment(JLabel.CENTER);
         selectGame.setHorizontalAlignment(JLabel.CENTER);
+        homeButton.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     private void addComponents(){
         add(title);
         add(selectGame);
+        add(homeButton);
+    }
+
+    public void addHomeButtonListener(ActionListener listener) {
+        homeButton.addActionListener(listener);
+    }
+
+    public void showHomeButton(boolean show) {
+        homeButton.setVisible(show);
+        selectGame.setVisible(!show);
+        revalidate();
+        repaint();
     }
 }
