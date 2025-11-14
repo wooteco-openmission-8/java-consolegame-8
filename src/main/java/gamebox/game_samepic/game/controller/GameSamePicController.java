fd61d47fde71a310315abad208bd2a37a498905d
K-KY <kyuyoungk@naver.com>
2025-11-14 21:45:14 +0900

    refactor : 게임이 진짜 시작될 때 이미지를 초기화 하도록 수정
    - 위 변경으로 재정의된 start를 사용하지 않음 논의 필요

--- src/main/java/gamebox/game_samepic/game/controller/GameSamePicController.java
+++ src/main/java/gamebox/game_samepic/game/controller/GameSamePicController.java
@@ -17,7 +17,6 @@ public class GameSamePicController implements Game {
 
     @Override
     public void start() {
-        gameSamePicService.initializePictures();
     }
 
     @Override
@@ -26,6 +25,7 @@ public class GameSamePicController implements Game {
     }
 
     public void start(Difficulty difficulty) {
+        gameSamePicService.initializePictures();
         int rows = difficulty.getRows();
         int cols = difficulty.getCols();
         gameSamePicService.newGame(rows, cols);
