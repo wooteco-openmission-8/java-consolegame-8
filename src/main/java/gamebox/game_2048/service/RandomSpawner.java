1fcb5bbcf280f38f43d5501036fce9dd8711a35a
K-KY <kyuyoungk@naver.com>
2025-11-19 19:42:51 +0900

    feat: 2또는 4를 무작위로 생성하는 기능 클래스로 분리

---
+++ src/main/java/gamebox/game_2048/service/RandomSpawner.java
@@ -0,0 +1,16 @@
+package gamebox.game_2048.service;
+
+import java.util.Random;
+
+public class RandomSpawner {
+    private static final Random random = new Random();
+    private static final double WEIGHT = 0.9;
+
+    public static int spawn() {
+            double probability = random.nextDouble();
+            if (probability > WEIGHT) {
+                return 2;
+            }
+            return 4;
+    }
+}
