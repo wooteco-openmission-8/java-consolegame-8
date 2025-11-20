235fb0fcffd1178037562ef555389ee32eed3e0c
jiseung <kimsz123456@gmail.com>
2025-11-20 17:58:16 +0900

    refactor: 게임 인터페이스 변경

--- src/main/java/gamebox/common/Game.java
+++ src/main/java/gamebox/common/Game.java
@@ -2,7 +2,14 @@ package gamebox.common;
 
 import gamebox.game_samepic.game.entity.Difficulty;
 
+/**
+ * 게임 인터페이스
+ */
 public interface Game {
+    /**
+     * 게임을 시작합니다
+     *
+     * @param difficulty 게임 난이도
+     */
     void start(Difficulty difficulty);
-    String getName();
 }
